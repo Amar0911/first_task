9b52ff9ea4a371a8d4cab0557e298549093e7cf9
Amar0911 <guptaamar0911@gmail.com>
2025-01-23 03:20:05 +0530

    update

---
+++ serializer/core/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('',views.marvel),
+    path('marvel/<int:id>',views.marvel_id),
+    path('marvel/',views.marvel_all)
+]
